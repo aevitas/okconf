d46826f4b048936a8fd11b1b75e7a78e3dacf91d
aevitas <m4rtijn@gmail.com>
2024-03-19 11:41:15 +0100

    update docs

--- config.go
+++ config.go
@@ -15,12 +15,12 @@ var (
 	errBuffNotWritten = errors.New("could not write full buffer to file; config may be corrupt on disk")
 )
 
-// loads the default configuration as returned by config.Default
+// loads the default state for configuration T, as returned by the configuration's Default method
 func Load[T Config]() *T {
 	return defaultCfg[T]()
 }
 
-// loads config from the specified JSON file, using the default configuration as a starting point
+// loads configuration of type T from the specified JSON file, applying it as a layer on top of T's Default configuration
 func FromJSON[T Config](file string) (*T, error) {
 	fp, err := filepath.Abs(file)
 	if err != nil {
@@ -37,7 +37,7 @@ func FromJSON[T Config](file string) (*T, error) {
 	return fromJSONStream[T](defaultCfg[T](), f)
 }
 
-// saves the
+// flattens and saves the configuration to the specified file
 func SaveJSON[T Config](cfg T, file string) error {
 	if !filepath.IsAbs(file) {
 		return errPathNotAbs
@@ -75,6 +75,7 @@ func fromJSONStream[T Config](conf *T, reader io.Reader) (*T, error) {
 	return conf, nil
 }
 
+// loads configuration of type T from the specified YAML file, applying it as a layer on top of T's Default configuration
 func FromYAML[T Config](file string) (*T, error) {
 	fp, err := filepath.Abs(file)
 	if err != nil {
@@ -108,6 +109,7 @@ func defaultCfg[T Config]() *T {
 	return &cfg
 }
 
+// base interface for okconf configuration
 type Config interface {
 	Default() Config
 }
